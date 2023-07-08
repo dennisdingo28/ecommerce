c6a1c55f49ca5bbae505852c8b45b3ac9fb331a5
Dennis Moldovan <dennismoldovan32@gmail.com>
2023-07-08 19:14:16 +0300

    cors fixes

--- actions/get-sizes.tsx
+++ actions/get-sizes.tsx
@@ -1,7 +1,10 @@
 import { Size } from "@/types";
 
-const URL = `${process.env.NEXT_PUBLIC_API_URL}/sizes`
+const URL=`${process.env.NEXT_PUBLIC_API_URL}/sizes`;
+
 export const getSizes = async (): Promise<Size[]> => {
-    const res = await fetch(URL);
-    return res.json();
-}
+  const res = await fetch(URL);
+
+  return res.json();
+};
+

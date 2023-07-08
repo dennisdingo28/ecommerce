49a982564922f678d2027c079f1041d0710b539c
Moldovan Dennis <dennismoldovan32@gmail.com>
2023-07-08 22:01:13 +0300

    cart done

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -8,6 +8,7 @@ import { Expand,ShoppingCart } from "lucide-react";
 import Currency from "@/components/ui/currency";
 import { useRouter } from "next/navigation";
 import usePreviewModal from "@/hooks/use-preview-modal";
+import useCart from "@/hooks/use-cart";
 
 interface ProductCard {
     data: Product;
@@ -16,6 +17,8 @@ interface ProductCard {
 
 
 const ProductCard:FC<ProductCard> = ({data}) => {
+    const cart = useCart();
+
     const previewModal = usePreviewModal();
     const router = useRouter();
     const handleClick = () =>{
@@ -25,6 +28,11 @@ const ProductCard:FC<ProductCard> = ({data}) => {
         event.stopPropagation();
         previewModal.onOpen(data);
     }
+    const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) =>{
+        event.stopPropagation();
+        cart.addItem(data);
+    }
+
   return (
     <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
         <div className="aspect-square rounded-xl bg-gray-100 relative">
@@ -32,7 +40,7 @@ const ProductCard:FC<ProductCard> = ({data}) => {
             <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
                 <div className="flex gap-x-6 justify-center">
                     <IconButton onClick={onPreview} icon={<Expand size={20} className="text-gray-600"/>}/>
-                    <IconButton onClick={()=>{}} icon={<ShoppingCart size={20} className="text-gray-600"/>}/>
+                    <IconButton onClick={onAddToCart} icon={<ShoppingCart size={20} className="text-gray-600"/>}/>
                 </div>
             </div>
         </div>
